d4b2b6316ef7d5e20febc3280c9cd25efb1a9a4c
Vitali Mazurenka <vitali.mazurenka@web.de>
2023-11-30 18:37:14 +0100

    add-enum-tupel-void-interfaceTheory+counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 //import Lesson20 from './lessons/Lesson20';
-import Homework20 from './homeworks/Homework20/Homework20';
+//import Homework20 from './homeworks/Homework20/Homework20';
+import Lesson21 from './lessons/Lesson21';
 
 import './App.css';
 
@@ -8,7 +9,8 @@ function App() {
     <div className="App">
     
     {/* <Lesson20/> */}
-    <Homework20/>
+     {/* <Homework20/> */}
+    <Lesson21/>
     </div>
   );
 }
